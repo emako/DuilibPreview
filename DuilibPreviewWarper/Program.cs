5cd0b5f01daa9c1aebae4d1d514c0f7312abe372
ema <mccoy39082@163.com>
2022-02-09 18:51:55 +0800

    added warper

---
+++ DuilibPreviewWarper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace DuilibPreviewWarper
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] _)
+        {
+            string tempDir = Path.Combine(Path.GetTempPath(), "DuilibPreviewWarper");
+            try
+            {
+                if (!Directory.Exists(tempDir))
+                {
+                    Directory.CreateDirectory(tempDir);
+                }
+                if (!File.Exists(Path.Combine(tempDir, "DuiLib_u.dll")))
+                {
+                    File.WriteAllBytes(Path.Combine(tempDir, "DuiLib_u.dll"), Resource.DuiLib_u);
+                }
+                if (!File.Exists(Path.Combine(tempDir, "DuilibPreview.exe")))
+                {
+                    File.WriteAllBytes(Path.Combine(tempDir, "DuilibPreview.exe"), Resource.DuilibPreview);
+                }
+            }
+            catch
+            {
+            }
+            Process.Start(Path.Combine(tempDir, "DuilibPreview.exe"));
+        }
+    }
+}
